fdca6de9d4d39a38ec7277351e4a95d989946162
joemarney <marney.joe@gmail.com>
2024-10-28 13:31:44 +0000

    setup file structure, App.jsx and auth.js

---
+++ src/utilities/auth.js
@@ -0,0 +1,21 @@
+const tokenName = "AUTH_TOKEN";
+
+export function setToken(token) {
+  localStorage.setItem(tokenName, token);
+}
+
+export function getToken() {
+  return localStorage.getItem(tokenName);
+}
+
+export function removeToken() {
+  localStorage.removeItem(tokenName);
+}
+
+export function getUser() {
+  const token = getToken();
+  if (!token) return null;
+
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  return payload;
+}
