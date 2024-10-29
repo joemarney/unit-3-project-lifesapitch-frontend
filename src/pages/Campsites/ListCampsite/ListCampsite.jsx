02bafa464752bcaf15dd17b9aa37102034eef7c8
the-mostest <fbmcdougall@gmail.com>
2024-10-29 17:06:32 +0000

    Stuck on useEffect to render camping images

--- src/pages/Campsites/ListCampsite/ListCampsite.jsx
+++ src/pages/Campsites/ListCampsite/ListCampsite.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 import { index } from '../../../services/campsiteService';
 
 // ! Pages
-// import AddCampsite from '../AddCampsite/AddCampsite';
+import AddCampsite from '../AddCampsite/AddCampsite';
 
 // ! Styles
 import styles from './ListCampsite.module.scss'
@@ -20,35 +20,40 @@ export default function ListCampsite() {
             try {
                 const { data } = await index()
                 setSites(data)
+                
             } catch (error) {
                 console.log(error);
             }
         }
         fetchSites()
-    }, [])
+    }, [setSites])
 
     return (
         <main className={styles.container}>
             <h1>Welcome to the Campsites</h1>
 
-            {/* <AddCampsite setSites={setSites} /> */}
+            <AddCampsite setSites={setSites} />
 
 
             <h3>Here you will find all of the campsites around the UK!</h3>
+            <ul>
 
-            {sites.map((site) => {
-                return (
-                    <Link key={site._id} to={`/campsites/${site._id}`}>
-                        <article>
-                            <header>
-                                <h2>{site.title}</h2>
-                            </header>
-                            <p>{site.cost} pp.pn</p>
-                            <p>{site.location}</p>
-                        </article>
-                    </Link>
-                )
-            })}
+                {sites.map((site) => {
+                    return (
+                        <li key={site._id} >
+                            <Link to={`/campsites/${site._id}`}>
+                                <article>
+                                    <header>
+                                        <h2>{site.title}</h2>
+                                    </header>
+                                    <p>{site.cost} pp.pn</p>
+                                    <p>{site.location}</p>
+                                </article>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
         </main>
     )
 }
