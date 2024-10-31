76541988c4c65d756633e150bcca94e51d28e803
joemarney <marney.joe@gmail.com>
2024-10-31 18:32:00 +0000

    bugfixes

--- src/pages/CheckList/Checklist.jsx
+++ src/pages/CheckList/Checklist.jsx
@@ -1,37 +1,51 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+// ! Styles
+import styles from "./Checklist.module.scss";
 
 export default function Checklist() {
-    const [items, setItems] = useState([])
-    const [newItem, setNewItem] = useState('')
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
 
-    function addItem(newItem) {
-        const updatedList = [...items, newItem]
-        setItems(updatedList)
-    }
+  function addItem(newItem) {
+    const updatedList = [...items, newItem];
+    setItems(updatedList);
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        addItem(newItem)
-        setNewItem('')
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    addItem(newItem);
+    setNewItem("");
+  }
 
-    function handleChange(event) {
-        setNewItem(event.target.value)
-    }
+  function handleChange(event) {
+    setNewItem(event.target.value);
+  }
 
-    return (
-        <main>
-        <h1>Checklist</h1>
+  return (
+    <main className={styles.container}>
+      <h1>Checklist</h1>
+      {items.length === 0 ? (
+        <h2>To help you pack your bag so you don't forget those all important items!</h2>
+      ) : (
         <ul>
-            {items.map((item, idx) => {
-                return <li key={idx}>{item}</li>
-            })}
+          {items.map((item, idx) => {
+            return (
+              <div key={idx} id="list-item">
+                <li>
+                  {item}
+                  <input type="checkbox"></input>
+                </li>
+              </div>
+            );
+          })}
         </ul>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='checklistInput'>Don't forget:</label>
-            <input type='text' name='item' onChange={handleChange} value={newItem} />
-            <button type='submit'>+</button>
-            </form>
-        </main>
-    )
+      )}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="checklistInput">Don't forget:</label>
+        <input type="text" name="item" onChange={handleChange} value={newItem} />
+        <button type="submit">+</button>
+      </form>
+    </main>
+  );
 }
