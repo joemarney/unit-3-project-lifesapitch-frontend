4016bcad798853bd1b6a57149a5d39df3ddbb3c6
joemarney <marney.joe@gmail.com>
2024-10-31 09:21:00 +0000

    added a carousel component

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+// ! Styles
+import styles from "../Carousel/Carousel.module.scss";
+
+export default function Carousel(props) {
+  const [activeIdx, setActiveIdx] = useState(0);
+
+  function nextSlide() {
+    setActiveIdx((previous) => (previous === props.images.length - 1 ? 0 : previous + 1));
+  }
+
+  function prevSlide() {
+    setActiveIdx((previous) => (previous === 0 ? props.images.length - 1 : previous - 1));
+  }
+
+  return (
+    <div className={styles.container}>
+      <button onClick={prevSlide}>-</button>
+      <img src={props.images[activeIdx]} alt={`Slide ${activeIdx}`} />
+      <button onClick={nextSlide}>+</button>
+    </div>
+  );
+}
